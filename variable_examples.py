22c42c8a479fed9001ddd78c90c103122a74205f
Zainab <zhaseeba1@appdev.yearup.org>
2024-10-21 15:19:59 -0500

    lab 2 question 1

---
+++ variable_examples.py
@@ -0,0 +1,10 @@
+CustomerID= '101256' #string since it would be a number created by the system most likely
+CustomerName = 'Emily Bria Watson' #String for text value
+Gender= 'Female' #string 
+DateofBirth= '04/05/06' #string
+DriverLicenseNumber= '' #string
+AutoPolicyNumber=''       #string
+
+myName='Zainab'
+myBirthCity= 'Chicago'
+myBirthState='Illinois'
