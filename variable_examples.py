c59ed230bc9fb31c1c46cc5fc75563944ec3740a
Zainab <zhaseeba1@appdev.yearup.org>
2024-10-21 15:57:39 -0500

    added more info for prob 2

--- variable_examples.py
+++ variable_examples.py
@@ -12,7 +12,10 @@ myBirthState='Illinois'
 #  Go to https://flexiple.com/python/python-reserved-words. Answer the following 
 # questions as comments at the bottom of variable_examples.py: 
 # ∗ What is the full list of reserved words that can’t be used for variable names?
-#False,def, if, raise,None, del,import,return,True, elif, in, try , and, else, is , while , as, except, lambda,with, assert,finally,nonlocal,yield,break, for,not,class, from, or, continue, global ,pass
+#False,def, if, raise,None, del,import,return,True, elif, 
+# in, try , and, else, is , while , as, except, lambda,
+# with, assert,finally,nonlocal,yield,break, for,not,
+# class, from, or, continue, global ,pass
 import keyword
 print (keyword.kwlist)
 
@@ -23,8 +26,8 @@ print (keyword.kwlist)
 # ∗ Pick 5 of these words and review the explanation for how it is used as a keyword in 
 # Python. Add these 5 definitions as comments to your exercise document. Put ^^ 
 # around any terms that you are not familiar with
-#1
-#2
-#3
-#4
-#5
+#1 false -check if false
+#2 if --test 
+#3 raise - when incorrect input
+#4 as -alias
+#5 true -- check if true
