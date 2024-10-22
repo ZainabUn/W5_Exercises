76e6192eee69accd3bec88633612f20e5651046d
Zainab <zhaseeba1@appdev.yearup.org>
2024-10-22 14:49:55 -0500

    next prob convert these into methods next

---
+++ MathScripts/taxes.py
@@ -0,0 +1,10 @@
+# Federal taxes are 23% of your salary every month. You make X amount of money. How
+# much is withheld for taxes?
+
+federal_tax_percent=23
+Salary_net= 1000
+
+
+tax_withheld= Salary_net*(federal_tax_percent/100)
+
+print(tax_withheld)
