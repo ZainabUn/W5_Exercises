2d102c7cdf51e3c26c05fd2df85d764d15ed527e
Zainab <zhaseeba1@appdev.yearup.org>
2024-10-22 14:41:56 -0500

    farenheit and celsius prob next lab

---
+++ MathScripts/c_to_f.py
@@ -0,0 +1,8 @@
+# °F = (°C × 9/5) + 32
+#How do you convert a temperature from Celsius to Fahrenheit?
+
+Celsius= 100
+
+Farenheit= (Celsius* 9/5) +32
+
+print(Celsius, 'in Celsius in Farenheit is', Farenheit)
---
+++ MathScripts/f_to_c.py
@@ -0,0 +1,7 @@
+# How do you convert a temperature from Fahrenheit to Celsius?
+
+Farenheit= 80
+
+Celsius= (Farenheit-32) *5/9
+
+print('Farenheit', Farenheit, 'in Celsius is', Celsius)
