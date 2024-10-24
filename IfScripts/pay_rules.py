c0271078903811ab13f6a142533f5f52ec8f43c6
Zainab <zhaseeba1@appdev.yearup.org>
2024-10-24 15:50:03 -0500

    still working on pay_rules eod

---
+++ IfScripts/pay_rules.py
@@ -0,0 +1,14 @@
+# In a file named pay_rules.py, create a script to calculate gross pay given the variables 
+# pay_rate and hours_worked. If the person works more than 40 hours, pay the 
+# overtime hours at 1.5 times the rate of regular hours. 
+pay_rate=input("enter payrate")
+hours_worked=input()
+
+
+
+
+
+# 2. When you are finished, review your script with a colleague. Are your algorithms 
+# similar? Do you believe each other’s code will work? 
+# 3. Run your script several times with different values for pay_rate and hours_worked and 
+# confirm the output is right.  
