3c0262363c6be2580a49fd44bf0bdfda05696c1c
Zainab <zhaseeba1@appdev.yearup.org>
2024-10-30 15:40:03 -0500

    work in progress min-max

---
+++ IfScripts/complex_taxes.py
@@ -0,0 +1,24 @@
+# Create a script named complex_taxes.py that will calculate federal tax based on the 
+# values of annual gross income (a number) and a filing status (‘single’ or ‘joint’). 
+# 2. Start by copying your code for calculation of gross pay from the earlier lab 
+# (pay_rules.py) and include it here as part of your starting point. Remember, that code 
+# calculates weekly gross pay. Extend that calculation to estimate annual gross pay 
+# (how many weeks in a year?) and save it to a new variable. 
+# 3. Use a series of if statements to determine the appropriate tax rate. 
+# The tax table for single filers is: 
+# The tax table for joint filers is: 
+# 4. Use the tax rate to determine the tax withheld from the weekly gross pay.  
+# 5. Create separate print statements to print the relevant information determined by the 
+# above calculations. The output of your script might look something like this: 
+# You worked __ hours this period. 
+# Because you earn $___ per hour, your gross weekly pay is $475 
+# Page 77 of 87 
+# Data Analyst Academy Week 5  
+# Student Guide 
+# Your filing status is ___ 
+# Your tax withholding for the week is $___ 
+# Your net pay is $___  
+# 6. To properly test the code, you will need to run it multiple times using different values 
+# for hours worked, pay rate, and filing status. Examine the tax tables and see if you can 
+# determine a set of good test values. Discuss with your small group – then run your 
+# tests!
---
+++ IfScripts/min_max.py
@@ -0,0 +1,20 @@
+#  Create a script named min_max.py that displays both the smallest and then the 
+# largest of three numbers. 
+# Name your variables a, b, and c and assign them values. Then use if/else statements 
+# to determine and display the answer. 
+# Be sure to test your script using an assortment of different values in your variables, so 
+# that you look at a variety of different number combinations.
+
+a=8;
+b=8;
+c=8;
+
+if a<b and a<c:
+    "a is greater"
+elif b<a and b<c:
+    "b is greater"
+elif c<a and c<b:
+    "c is greater"
+elif a==b or 
+else:
+    print('check input again. ')
